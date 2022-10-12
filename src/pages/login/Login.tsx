981757201dcc8194acd0c06693f5cab3684217dc
ROGU3N1NJA <smeershoek2006@gmail.com>
2022-10-12 21:40:45 +0200

    styled login screen

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './Login.css';
 
 interface Props {
     onSubmit: (school: string, code: string) => void
@@ -11,9 +12,10 @@ export function Login({onSubmit}: Props) {
   return (
     <div className="login">
         <form onSubmit={(e) => {e.preventDefault(); onSubmit(school, code)}}>
-            <input value={school} onChange={(e) => setSchool(e.target.value)} type="text" required/>
-            <input value={code} onChange={(e) => setCode(e.target.value.replace(/\s+/g, ''))} type="text" required/>
-            <button type="submit">Login</button>
+          <h1>Login</h1>
+          <input value={school} onChange={(e) => setSchool(e.target.value)} type="text" required/>
+          <input value={code} onChange={(e) => setCode(e.target.value.replace(/\s+/g, ''))} type="text" required/>
+          <button type="submit">Login</button>
         </form> 
     </div>
    
