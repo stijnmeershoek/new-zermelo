79f68a963ee9b8cebb922f0139fddeaf9563ef37
ROGU3N1NJA <smeershoek2006@gmail.com>
2022-10-14 22:38:10 +0200

    fixed bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), VitePWA({
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
     manifest: {
