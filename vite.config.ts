cc612a2b8b2bbdc93ca1ae0b9983445b82279524
ROGU3N1NJA <smeershoek2006@gmail.com>
2022-10-14 22:43:05 +0200

    fixed bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/new-zermelo/',
   plugins: [react(), VitePWA({
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
     manifest: {
+      start_url: "/new-zermelo/",
       name: 'Zermelo',
       short_name: 'Zermelo',
       description: 'Zermelo but Better',
